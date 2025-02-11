7a54cbd004217e92c133b78a63b6a2fd2d7a86fe
dariia-shevchuk <117785718+dariia-shevchuk@users.noreply.github.com>
2025-02-11 09:46:59 +0100

    fix

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -95,23 +95,10 @@ export function LoginForm({
               Login
             </Button>
           </div>
-          <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
-            <span className="relative z-10 bg-background px-2 text-muted-foreground">
-              Or
-            </span>
-          </div>
-          <div className="grid gap-4 sm:grid-cols-1">
-          <div className="text-center text-sm">
-              Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
-                Sign up
-              </a>
-            </div>
-          </div>
         </div>
       {/* </form> */}
       <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary  ">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
+        By clicking login, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
       </div>
       {errorMessage && <div className="text-red-500">{errorMessage}</div>}
